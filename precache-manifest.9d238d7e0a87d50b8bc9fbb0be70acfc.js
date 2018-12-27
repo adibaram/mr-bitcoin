6a4f95d18622c27d559b39492240b87402cd8c70
Adi Baram <adididi@gmail.com>
2018-12-27 11:17:19 +0200

    Updates

---
+++ precache-manifest.9d238d7e0a87d50b8bc9fbb0be70acfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95706a4c4e6ddf5388f1",
+    "url": "/mr-bitcoin/static/css/main.5096d84a.chunk.css"
+  },
+  {
+    "revision": "95706a4c4e6ddf5388f1",
+    "url": "/mr-bitcoin/static/js/main.95706a4c.chunk.js"
+  },
+  {
+    "revision": "bbf7a2c5dba806f5b2da",
+    "url": "/mr-bitcoin/static/js/1.bbf7a2c5.chunk.js"
+  },
+  {
+    "revision": "725ba66c854ee4b5ddf4",
+    "url": "/mr-bitcoin/static/js/runtime~main.725ba66c.js"
+  },
+  {
+    "revision": "87dcf9227b66043c6c3ac3c48b96b24e",
+    "url": "/mr-bitcoin/index.html"
+  }
+];
